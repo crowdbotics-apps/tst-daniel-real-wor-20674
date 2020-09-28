b54ece576bca205c604a9fa994bbb15c12820db8
Crowdbotics <team@crowdbotics.com>
2020-09-28 18:14:33 +0000

    Update store

--- src/store/services.js
+++ src/store/services.js
@@ -1,1 +1,6 @@
 import axios from "axios"
+const swaggerPetStore = axios.create({
+  baseURL: "https://petstore.swagger.io/v2",
+  headers: { Accept: "application/json", "Content-Type": "application/json" }
+})
+export const apiService = {}
