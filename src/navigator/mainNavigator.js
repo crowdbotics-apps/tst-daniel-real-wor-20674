55409e503223c9c8daa7137563dbb28dcf872af3
Daniel <daniel.s@crowdbotics.com>
2020-10-08 16:39:37 +0100

    Remove all the unused extra files

--- src/navigator/mainNavigator.js
+++ src/navigator/mainNavigator.js
@@ -1,27 +1,13 @@
 import {createAppContainer} from 'react-navigation';
 import {createDrawerNavigator} from 'react-navigation-drawer';
 
-import SplashScreen from '../features/SplashScreen';
-import SideMenu from './sideMenu';
 import PostsNavigator from '../features/BlankScreen0120547/navigator';
 
 const AppNavigator = {
   Posts: {
     screen: PostsNavigator,
   },
-  SplashScreen: {
-    screen: SplashScreen,
-  },
 };
-
-const DrawerAppNavigator = createDrawerNavigator(
-  {
-    ...AppNavigator,
-  },
-  {
-    contentComponent: SideMenu,
-  },
-);
 
 const AppContainer = createAppContainer(DrawerAppNavigator);
 
